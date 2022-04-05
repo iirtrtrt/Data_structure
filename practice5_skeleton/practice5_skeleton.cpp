16cade5c29b0d539755536a6f92d67a146b2e7f2
9043820226 <9043820226@hanyang.ac.kr>
2022-04-05 10:02:02 +0900

    tree orders

--- practice5_skeleton/practice5_skeleton.cpp
+++ practice5_skeleton/practice5_skeleton.cpp
@@ -20,104 +20,117 @@ const char INORDER = 'N';
 const char PREORDER = 'R';
 const char POSTORDER = 'O';
 
-class TreeNode {
+class TreeNode
+{
 public:
   int key;
-  TreeNode* left;
-  TreeNode* right;
-  TreeNode(int k, TreeNode* l=nullptr, TreeNode* r=nullptr) {
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int k, TreeNode *l = nullptr, TreeNode *r = nullptr)
+  {
     key = k;
     left = l;
     right = r;
   }
 };
 
-class BinarySearchTree {
+class BinarySearchTree
+{
 public:
-  TreeNode* root;
-  BinarySearchTree(): root(nullptr) {}
+  TreeNode *root;
+  BinarySearchTree() : root(nullptr) {}
   ~BinarySearchTree();
 
-  TreeNode* arrayToBST(vector<int>&, int, int);
+  TreeNode *arrayToBST(vector<int> &, int, int);
   // Return true if tree is empty; false otherwise
   bool isEmpty() { return (root == nullptr); }
-  TreeNode* findMin();
-  TreeNode* findMax();
-  TreeNode* search(int);
-  void writeInorder(ofstream&);
-  void writePreorder(ofstream&);
-  void writePostorder(ofstream&);
+  TreeNode *findMin();
+  TreeNode *findMax();
+  TreeNode *search(int);
+  void writeInorder(ofstream &, TreeNode *);
+  void writePreorder(ofstream &, TreeNode *);
+  void writePostorder(ofstream &, TreeNode *);
   void printTree();
+
 private:
-  int _getHeight(TreeNode*);
-  void _printSpaces(double, TreeNode*);
+  int _getHeight(TreeNode *);
+  void _printSpaces(double, TreeNode *);
 };
 
-BinarySearchTree::~BinarySearchTree() {
+BinarySearchTree::~BinarySearchTree()
+{
   // Practice 4
-  vector<TreeNode*> stack;
-  vector<TreeNode*> nodes;
-  TreeNode* curr = root;
-  TreeNode* prev = nullptr;
-  while (curr != nullptr || !stack.empty()) {
-    while (curr != nullptr) {
+  vector<TreeNode *> stack;
+  vector<TreeNode *> nodes;
+  TreeNode *curr = root;
+  TreeNode *prev = nullptr;
+  while (curr != nullptr || !stack.empty())
+  {
+    while (curr != nullptr)
+    {
       stack.push_back(curr);
       curr = curr->left;
     }
     curr = stack.back();
-    if (curr->right == nullptr || curr->right == prev) {
+    if (curr->right == nullptr || curr->right == prev)
+    {
       stack.pop_back();
       nodes.push_back(curr);
       prev = curr;
       curr = nullptr;
     }
-    else {
+    else
+    {
       curr = curr->right;
     }
   }
-  vector<TreeNode*>::iterator it;
+  vector<TreeNode *>::iterator it;
   for (it = nodes.begin(); it != nodes.end(); ++it)
     delete *it;
 }
 
-// Given a sequence arr of integers, start index l, the end index r, 
+// Given a sequence arr of integers, start index l, the end index r,
 // build a binary search (sub)tree that contains keys in arr[l], ..., arr[r].
 // Return the root node of the tree
-TreeNode* BinarySearchTree::arrayToBST(vector<int>& arr, int l, int r) {
+TreeNode *BinarySearchTree::arrayToBST(vector<int> &arr, int l, int r)
+{
   // Practice 4
   if (l > r)
     return nullptr;
   vector<int>::iterator it = arr.begin();
   int prev = *it;
-  for(; it != arr.end(); ++it) {
+  for (; it != arr.end(); ++it)
+  {
     if (prev > *it)
       return nullptr;
     prev = *it;
   }
   int mid = (l + r) / 2;
-  TreeNode* node = new TreeNode(arr[mid]);
+  TreeNode *node = new TreeNode(arr[mid]);
   node->left = arrayToBST(arr, l, mid - 1);
   node->right = arrayToBST(arr, mid + 1, r);
   return node;
 }
 
-// Return the node with the minimum value 
-TreeNode* BinarySearchTree::findMin() {
+// Return the node with the minimum value
+TreeNode *BinarySearchTree::findMin()
+{
   // Practice 4
   if (isEmpty())
     return nullptr;
-  TreeNode* p = root;
+  TreeNode *p = root;
   while (p->left != nullptr)
     p = p->left;
   return p;
 }
 
-// Return the node with the maximum value 
-TreeNode* BinarySearchTree::findMax() {
+// Return the node with the maximum value
+TreeNode *BinarySearchTree::findMax()
+{
   // Practice 4
   if (isEmpty())
     return nullptr;
-  TreeNode* p = root;
+  TreeNode *p = root;
   while (p->right != nullptr)
     p = p->right;
   return p;
@@ -126,80 +139,133 @@ TreeNode* BinarySearchTree::findMax() {
 // Given a query, search for the node whose key is equal to query.
 // If the node exists, return the key
 // Otherwise, return nullptr
-TreeNode* BinarySearchTree::search(int query) {
+TreeNode *BinarySearchTree::search(int query)
+{
   // TODO. Practice 5
+  TreeNode *ptr = root;
+  while (ptr)
+  {
+    if (ptr->key == query)
+    {
+      return ptr;
+    }
+    else if (ptr->key < query)
+    {
+      ptr = ptr->right;
+    }
+    else if (ptr->key > query)
+    {
+      ptr = ptr->left;
+    }
+  }
+
+  return 0;
 }
 
-// Given an output file stream, write the keys of all the nodes 
+// Given an output file stream, write the keys of all the nodes
 // visited in inorder traversal
-void BinarySearchTree::writeInorder(ofstream& outFile) {
+void BinarySearchTree::writeInorder(ofstream &outFile, TreeNode *ptr)
+{
   // TODO. Practice 5
+  if (ptr == nullptr)
+  {
+    return;
+  }
+  writeInorder(outFile, ptr->left);
+  outFile << ptr->key << " ";
+  writeInorder(outFile, ptr->right);
 }
 
-// Given an output file stream, write the keys of all the nodes 
+// Given an output file stream, write the keys of all the nodes
 // visited in preorder traversal
-void BinarySearchTree::writePreorder(ofstream& outFile) {
+void BinarySearchTree::writePreorder(ofstream &outFile, TreeNode *ptr)
+{
   // TODO. Practice 5
+  if (ptr == nullptr)
+  {
+    return;
+  }
+  outFile << ptr->key << " ";
+  writePreorder(outFile, ptr->left);
+  writePreorder(outFile, ptr->right);
 }
 
-// Given an output file stream, write the keys of all the nodes 
+// Given an output file stream, write the keys of all the nodes
 // visited in postorder traversal
-void BinarySearchTree::writePostorder(ofstream& outFile) {
+void BinarySearchTree::writePostorder(ofstream &outFile, TreeNode *ptr)
+{
   // TODO. Practice 5
+  if (ptr == nullptr)
+  {
+    return;
+  }
+  writePostorder(outFile, ptr->left);
+  writePostorder(outFile, ptr->right);
+  outFile << ptr->key << " ";
 }
 
-int BinarySearchTree::_getHeight(TreeNode* curr) {
-  if (curr == nullptr) 
+int BinarySearchTree::_getHeight(TreeNode *curr)
+{
+  if (curr == nullptr)
     return 0;
   return 1 + max(_getHeight(curr->left), _getHeight(curr->right));
 }
 
-void BinarySearchTree::_printSpaces(double n, TreeNode* curr) {
-  for(double i = 0; i < n; ++i) 
-    cout<<"  ";
+void BinarySearchTree::_printSpaces(double n, TreeNode *curr)
+{
+  for (double i = 0; i < n; ++i)
+    cout << "  ";
   if (curr == nullptr)
-    cout<<" ";
+    cout << " ";
   else
-    cout<<curr->key;
+    cout << curr->key;
 }
 
-void BinarySearchTree::printTree() {
+void BinarySearchTree::printTree()
+{
   if (root == nullptr)
     return;
-  queue<TreeNode*> q;
+  queue<TreeNode *> q;
   q.push(root);
-  queue<TreeNode*> temp;
+  queue<TreeNode *> temp;
   int cnt = 0;
   int height = _getHeight(root) - 1;
   double nMaxNodes = pow(2, height + 1) - 1;
-  while (cnt <= height) {
-    TreeNode* curr = q.front();
+  while (cnt <= height)
+  {
+    TreeNode *curr = q.front();
     q.pop();
     if (temp.empty())
       _printSpaces(nMaxNodes / pow(2, cnt + 1) + height - cnt, curr);
     else
       _printSpaces(nMaxNodes / pow(2, cnt), curr);
-    if (curr == nullptr) {
+    if (curr == nullptr)
+    {
       temp.push(nullptr);
       temp.push(nullptr);
     }
-    else {
+    else
+    {
       temp.push(curr->left);
       temp.push(curr->right);
     }
-    if (q.empty()) {
-      cout<<endl<<endl;
+    if (q.empty())
+    {
+      cout << endl
+           << endl;
       q = temp;
-      queue<TreeNode*> empty;
+      queue<TreeNode *> empty;
       swap(temp, empty);
       ++cnt;
     }
   }
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    cerr<<"Correct usage: [exe] [input] [output]"<<endl;
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    cerr << "Correct usage: [exe] [input] [output]" << endl;
     exit(1);
   }
   BinarySearchTree tree;
@@ -211,62 +277,92 @@ int main(int argc, char* argv[]) {
     char op = line[0];
     istringstream iss(line.substr(1));
     int k;
-    TreeNode* found = nullptr;
+    TreeNode *found = nullptr;
     vector<int> data;
-    switch(op)
+    switch (op)
     {
-      case BUILD:
-        while (iss >> k)
-          data.push_back(k);
-        tree.root = tree.arrayToBST(data, 0, data.size() - 1);
-        if (tree.root) {
-          outFile<<BUILD<<endl;
-        }
-        else {
-          cerr<<"BUILD: invalid input"<<endl;
-          exit(1);
-        }
-        tree.printTree();
-        break;
-      case FIND_MIN:
-        found = tree.findMin();
-        if (found == nullptr) {
-          cerr<<"FindMin failed"<<endl;
-          exit(1);
-        }
-        else {
-          outFile<<found->key<<endl;
-        }
-        break;
-      case FIND_MAX:
-        found = tree.findMax();
-        if (found == nullptr) {
-          cerr<<"FindMax failed"<<endl;
-          exit(1);
-        }
-        else {
-          outFile<<found->key<<endl;
-        }
-        break;
-      case SEARCH:
-        if (!(iss >> k)) {
-          cerr<<"SEARCH: invalid input"<<endl;
-          exit(1);
-        }
-        // TODO. Practice 5. Call the function for search
-        break;
-      case INORDER:
-        // TODO. Practice 5. Call the function for inorder traversal;
-        break;
-      case PREORDER:
-        // TODO. Practice 5. Call the function for preorder traversal;
-        break;
-      case POSTORDER:
-        // TODO. Practice 5. Call the function for postorder traversal;
-        break;
-      default:
-        cerr<<"Undefined operator"<<endl;
+    case BUILD:
+      while (iss >> k)
+        data.push_back(k);
+      tree.root = tree.arrayToBST(data, 0, data.size() - 1);
+      if (tree.root)
+      {
+        outFile << BUILD << endl;
+      }
+      else
+      {
+        cerr << "BUILD: invalid input" << endl;
         exit(1);
+      }
+      tree.printTree();
+      break;
+    case FIND_MIN:
+      found = tree.findMin();
+      if (found == nullptr)
+      {
+        cerr << "FindMin failed" << endl;
+        exit(1);
+      }
+      else
+      {
+        outFile << found->key << endl;
+      }
+      break;
+    case FIND_MAX:
+      found = tree.findMax();
+      if (found == nullptr)
+      {
+        cerr << "FindMax failed" << endl;
+        exit(1);
+      }
+      else
+      {
+        outFile << found->key << endl;
+      }
+      break;
+    case SEARCH:
+      if (!(iss >> k))
+      {
+        cerr << "SEARCH: invalid input" << endl;
+        exit(1);
+      }
+      // TODO. Practice 5. Call the function for search
+      if (tree.search(k))
+      {
+        outFile << tree.search(k)->key << endl;
+      }
+      else
+      {
+        cerr << "Search Failed" << endl;
+      }
+      break;
+    case INORDER:
+      // TODO. Practice 5. Call the function for inorder traversal;
+      if (tree.search(k))
+      {
+        tree.writeInorder(outFile, tree.root);
+        outFile << endl;
+      }
+      break;
+    case PREORDER:
+      // TODO. Practice 5. Call the function for preorder traversal;
+      if (tree.search(k))
+      {
+        tree.writePreorder(outFile, tree.root);
+        outFile << endl;
+      }
+      break;
+    case POSTORDER:
+      // TODO. Practice 5. Call the function for postorder traversal;
+      if (tree.search(k))
+      {
+        tree.writePostorder(outFile, tree.root);
+        outFile << endl;
+      }
+      break;
+    default:
+      cerr << "Undefined operator" << endl;
+      exit(1);
     }
   }
   outFile.close();

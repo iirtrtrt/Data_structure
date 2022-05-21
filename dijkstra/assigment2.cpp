0fd8804221d44fc20d94d440e3a45572baac0520
9043820226 <9043820226@hanyang.ac.kr>
2022-05-22 01:59:13 +0900

    from geeks

--- dijkstra/assigment2.cpp
+++ dijkstra/assigment2.cpp
@@ -34,12 +34,14 @@ public:
     void setVertical(int);
     void addEdge(vector<tuple<int, int, int>> &);
     void dijkstra(int, int);
+    int minDistance(int[], bool[]);
+    void printSolution(int[], int);
 };
 
 void Graph::setVertical(int nVertices)
 {
     this->nVertices = nVertices;
-    adj.resize(nVertices, vector<int>(nVertices, -1));
+    adj.resize(nVertices, vector<int>(nVertices, 0));
 }
 
 void Graph::addEdge(vector<tuple<int, int, int>> &data)
@@ -57,62 +59,62 @@ void Graph::addEdge(vector<tuple<int, int, int>> &data)
     }
 }
 
+int Graph::minDistance(int dist[], bool sptSet[])
+{
+    int min = 999999999;
+    int min_index;
+
+    for (int v = 0; v < nVertices; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
+}
+
+void Graph::printSolution(int dist[], int n)
+{
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < nVertices; i++)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
+
 void Graph::dijkstra(int s, int e)
 {
     priority_queue<pair<string, int>, vector<pair<string, int>>, shortest> pq;
 
-    // for (auto i = 0; i < adj.size(); i++)
-    // {
-    //     for (int j = 0; j < adj[i].size(); j++)
-    //     {
-    //         cout << adj[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
+    int dist[nVertices]; // The output array.  dist[i] will hold the shortest
+    // distance from src to i
 
-    // vector<bool> visited;
-    // visited.resize(nVertices, false);
-    // list<int> queue;
+    bool sptSet[nVertices]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
 
-    // visited[s] = true;
-    // queue.push_back(s);
+    // Initialize all distances as INFINITE and stpSet[] as false
+    for (int i = 0; i < nVertices; i++)
+        dist[i] = 999999999, sptSet[i] = false;
 
-    // while (!queue.empty())
-    // {
-    //     s = queue.front();
-    //     cout << s << " ";
-    //     queue.pop_front();
+    // Distance of source vertex from itself is always 0
+    dist[s] = 0;
 
-    //     for (auto adjecent : adj[s])
-    //     {
-    //         if (!visited[adjecent])
-    //         {
-    //             visited[adjecent] = true;
-    //             queue.push_back(adjecent);
-    //         }
-    //     }
-    // }
-    // cout << endl;
+    // Find shortest path for all vertices
+    for (int count = 0; count < nVertices - 1; count++)
+    {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        int u = minDistance(dist, sptSet);
+        cout << "What is u : " << u << endl;
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
 
-    // for (int i = 0; i < nVertices; i++)
-    // {
-    //     for (int j = 0; i < nVertices; j++)
-    //     {
-    //         vector<bool> visited;
-    //         visited.resize(nVertices, false);
-    //         list<int> queue;
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < nVertices; v++)
 
-    //         visited[s] = true;
-    //         queue.push_back(s);
-    //         s = queue.front();
-    //         cout << s << " ";
-    //         queue.pop_front();
-    //         for (int l = 0; l < nVertices; l++)
-    //         {
-    //         }
-    //     }
-    //     cout << endl;
-    // }
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && adj[u][v] && dist[u] != 999999999 && dist[u] + adj[u][v] < dist[v])
+                dist[v] = dist[u] + adj[u][v];
+    }
+    printSolution(dist, nVertices);
 }
 
 int main(int argc, char *argv[])
